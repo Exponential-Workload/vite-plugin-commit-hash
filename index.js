6b41f1066ec8bdf9cbab10a5f7e7194c96a3809a
Exponential-Workload <cc@aaathats3as.com>
2023-02-02 16:53:06 +0100

    fix: no import sadge

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {execSync} from 'child_process';
+const {execSync} = require('child_process');
 const getCommit = ()=>encodeURIComponent(execSync('git describe --long --always --dirty --abbrev=10000').toString().trim())
 let commit = getCommit();
 let commitExpires = Date.now() + 10000
