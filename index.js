01f5af15c15a61e19d4653fee8d1d3f21b15e81c
ExponentialWorkload <90570076+Exponential-Workload@users.noreply.github.com>
2023-02-02 16:44:53 +0100

    feat: Add Index

---
+++ index.js
@@ -0,0 +1,39 @@
+// plugin
+import {execSync} from 'child_process';
+const getCommit = ()=>encodeURIComponent(execSync('git describe --long --always --dirty --abbrev=10000').toString().trim())
+let commit = getCommit();
+let commitExpires = Date.now() + 10000
+const virtualModuleId = 'virtual:commit-hash'
+const resolvedVirtualModuleId = '\0'+virtualModuleId
+const cachedCommitId = ()=>{
+	if (Date.now() > commitExpires) {
+		// ensure if you commit, it updates
+		commit = getCommit();
+		commitExpires = Date.now() + 10000
+	}
+	return commit
+}
+const plugin = (config={noPrefix:false,noVirtual:false}) => {
+	return {
+		name: 'ExponentialWorkload: Git Commit Hash',
+		transform: config.noPrefix ? undefined : (str,id) => {
+			const commit = cachedCommitId()
+			const filename = id.split('?')[0]
+			if (filename.endsWith('.css'))
+				str=`/** Commit ${commit} **/\n`+str
+			else if (filename.endsWith('.js'))
+				str=`// Commit ${commit}\n`+str
+			return str;
+		},
+		resolveId(id) {
+      if (!config.noVirtual && id === virtualModuleId) {
+        return resolvedVirtualModuleId
+      }
+    },
+    load(id) {
+      if (!config.noVirtual && id === resolvedVirtualModuleId) {
+        return `export const id = '${cachedCommitId()}';export default id;`
+      }
+    },
+	}
+}
