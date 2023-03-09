7781af1cd2f5cc1b58b15d2bef40510da7c8b59a
Exponential-Workload <cc@aaathats3as.com>
2023-03-09 01:25:34 +0000

    fix: Fix stuff

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,46 @@
-import {execSync} from 'child_process';
-const getCommit = ()=>encodeURIComponent(execSync('git describe --long --always --dirty --abbrev=10000').toString().trim())
+import { execSync } from 'child_process';
+const getCommit = () => {
+  try {
+    return encodeURIComponent(execSync('git describe --long --always --dirty --abbrev=10000').toString().trim())
+  } catch (error) {
+    console.warn(error);
+    return 'unknown-commit'
+  }
+}
 let commit = getCommit();
 let commitExpires = Date.now() + 10000
 const virtualModuleId = 'virtual:commit-hash'
-const resolvedVirtualModuleId = '\0'+virtualModuleId
-const cachedCommitId = ()=>{
-	if (Date.now() > commitExpires) {
-		// ensure if you commit, it updates
-		commit = getCommit();
-		commitExpires = Date.now() + 10000
-	}
-	return commit
+const resolvedVirtualModuleId = '\0' + virtualModuleId
+const cachedCommitId = () => {
+  if (Date.now() > commitExpires) {
+    // ensure if you commit, it updates
+    commit = getCommit();
+    commitExpires = Date.now() + 10000
+  }
+  return commit
 }
-export const CommitHashPlugin = (config={noPrefix:false,noVirtual:false}) => {
-	return {
-		name: 'ExponentialWorkload: Git Commit Hash',
-		transform: config.noPrefix ? undefined : (str,id) => {
-			const commit = cachedCommitId()
-			const filename = id.split('?')[0]
-			if (filename.endsWith('.css'))
-				str=`/** Commit ${commit} **/\n`+str
-			else if (filename.endsWith('.js'))
-				str=`// Commit ${commit}\n`+str
-			return str;
-		},
-		resolveId(id) {
-			if (!config.noVirtual && id === virtualModuleId) {
-				return resolvedVirtualModuleId
-			}
-		},
-		load(id) {
-			if (!config.noVirtual && id === resolvedVirtualModuleId) {
-				return `export const id = '${cachedCommitId()}';export default id;`
-			}
-		},
-	}
+export const CommitHashPlugin = (config = { noPrefix: false, noVirtual: false }) => {
+  return {
+    name: 'ExponentialWorkload: Git Commit Hash',
+    transform: config.noPrefix ? undefined : (str, id) => {
+      const commit = cachedCommitId()
+      const filename = id.split('?')[0]
+      if (filename.endsWith('.css'))
+        str = `/** Commit ${commit} **/\n` + str
+      else if (filename.endsWith('.js'))
+        str = `// Commit ${commit}\n` + str
+      return str;
+    },
+    resolveId(id) {
+      if (!config.noVirtual && id === virtualModuleId) {
+        return resolvedVirtualModuleId
+      }
+    },
+    load(id) {
+      if (!config.noVirtual && id === resolvedVirtualModuleId) {
+        return `export const id = '${cachedCommitId()}';export default id;`
+      }
+    },
+  }
 }
 export default CommitHashPlugin
